28b9c24d7464d9b3cf8d88acfb870a66ccb87aac
stanislaw <stanislaw.kochanski@protonmail.com>
2024-06-04 23:03:26 +0200

    refactor: remove django

--- src/accounts_management/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountManagementConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "accounts_management"
--- src/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("accounts_management.urls")),
-    path("analysis/", include("budget_analysis.urls")),
-    path("notifications/", include("users_notifications.urls")),
-]
